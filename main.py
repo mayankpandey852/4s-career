04642e2f52ec1416e5bfd8f1552706d06bbcd84f
mayankpandey852 <68373245+mayankpandey852@users.noreply.github.com>
2023-08-02 12:27:10 +0000

    update on 02-08-2023

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app =Flask(__name__)
 
+Jobs = [
+  {
+   'id':124,
+    'name':'Data Analyst',
+    'Location' : 'Bangalore',
+    'salary' : '$5444'
+    
+  },
+  {
+   'id':545,
+    'name':'SDE',
+    'Location' : 'Bangalore',
+    'salary' : '$5444'
+    
+  },
+  {
+   'id':548,
+    'name':'Java Developer',
+    'Location' : 'Bangalore',
+    'salary' : '$45510'
+    
+  },
+  {
+   'id':369,
+    'name':'Web Developer',
+    'Location' : 'Bangalore',
+    'salary' : '$8000'
+    
+  },
+  {
+   'id':184,
+    'name':'UX/UI Designer',
+    'Location' : 'Bangalore',
+   
+    
+  }
+  
+]
 
 @app.route("/")
 def hello():
-  return "HELLO WORLD"
+ return render_template('index.html',
+                       jobs = Jobs,
+                       company_name="4S" 
+                      )
 if __name__ == "__main__" :
   app.run(host="0.0.0.0" , debug="True")
